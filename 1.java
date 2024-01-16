c9c7f0594f97e2460dd8f8723cd5c852db5e02ee
lniamien <149144539+lniamien@users.noreply.github.com>
2024-01-16 16:00:04 +0000

    Test MR

---
+++ 1.java
@@ -0,0 +1,15 @@
+package objet;
+
+	public class Tableau {
+
+		public static void main(String[] args) {
+
+
+			String[][] tableau1 = new String[10][20];
+
+			//rang 9 siege 19
+
+			tableau1[9][19]= "Leandre Niamien";
+				
+		}
+	}
